e2f33acf49033bdc9701d9e7e31bf074b1483d4c
NombreAf <nahuelramirez20179@gmail.com>
2024-11-29 16:59:23 -0400

    Basket docker and redis repository

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Please provide a valid cart");
+
+        RuleFor(x => x.Cart.UserName).NotNull().WithMessage("Please provide a valid name");
+    }
+}
+
+public class StoreBasketCommandHandler(IBasketRepository repository)
+    : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+
+       await repository.StoreBasket(command.Cart, cancellationToken);
+       
+       return new StoreBasketResult(command.Cart.UserName); 
+    }
+}
---
+++ src/Services/Basket/Basket.API/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Basket.API.Exceptions;
+
+public class BasketNotFoundException: NotFoundException
+{
+    public BasketNotFoundException(string userName) : base("Basket", userName)
+    {
+    }
+
+    public BasketNotFoundException(string name, object key) : base(name, key)
+    {
+    }
+}
