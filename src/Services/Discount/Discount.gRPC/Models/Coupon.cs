9cc305ccaf199766c6140202d934cce7bc80ec4c
NombreAf <nahuelramirez20179@gmail.com>
2024-12-10 10:13:18 -0400

    gRPC Microservice

---
+++ src/Services/Discount/Discount.gRPC/Models/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace Discount.gRPC.Models;
+
+public class Coupon
+{
+    public int Id { get; set; }
+    
+    public string ProductName { get; set; } = default!;
+    
+    public string ProductDescription { get; set; } = default!;
+    
+    public decimal Amount { get; set; }
+}
